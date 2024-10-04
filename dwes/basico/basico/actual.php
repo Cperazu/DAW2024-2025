fdde207f7181fd01d1ac74dc84b222f6fa67099a
cperazu <cperazu@gmail.com>
2024-10-04 14:19:01 +0200

    carpeta extra basico eliminada

--- dwes/basico/basico/actual.php
+++
@@ -1,9 +0,0 @@
-<?php declare (strict_types=1);
-
-$nombre = $_POST['nombre'] . " iesportada";
-$edad = (int)$_POST['edad'] + 5;
-
-$result =["nombre" => $nombre, "edad" => $edad];
-
-echo json_encode($result);
-
